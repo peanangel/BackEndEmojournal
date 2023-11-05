60bb75b038dd173ef25c221d2caa181df8f0eccd
peanangel <64011212214@msu.ac.th>
2023-11-05 19:14:59 +0700

    first commit

---
+++ src/main/java/com/example/diaryProject/DiaryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.diaryProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryProjectApplication.class, args);
+	}
+
+}
